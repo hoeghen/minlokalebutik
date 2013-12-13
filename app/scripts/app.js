2b09202329d9a240fc18d1a4ceecc15f37ce82d7
hoeghen <carverdk@yahoo.dk>
2013-12-13 16:51:17 +0100

    login and butik

---
+++ app/scripts/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('testappApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/butik', {
+        templateUrl: 'views/butik.html',
+        controller: 'ButikCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ dist/scripts/31d62ad1.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("testappApp",["ngCookies","ngResource","ngSanitize","ngRoute"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("testappApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
