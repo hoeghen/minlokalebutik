e7669401b97902fca811d54bb3e5ccbb095e9620
hoeghen <carverdk@yahoo.dk>
2014-01-24 16:20:23 +0100

    working css

--- Gruntfile.js
+++ Gruntfile.js
@@ -247,7 +247,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '.htaccess',
             'bower_components/**/*',
-            'images/{,*/}*.{webp}',
+            'images/*',
             'fonts/*'
           ]
         }, {
