23597250c6d40f9463c22157bea26ddb33c7dafc
hoeghen <carverdk@yahoo.dk>
2014-03-01 11:20:25 +0100

    flash is working

--- dist/bower_components/angular-flash-messages/angular-flash.js
+++ dist/bower_components/angular-flash-messages/angular-flash.js
@@ -1,70 +1,58 @@
 angular.module('flash', [])
-  .factory('flash', ['$rootScope', '$timeout', function ($rootScope, $timeout) {
-    var messages = [];
-    var reset;
+.factory('flash', ['$rootScope', '$timeout', function($rootScope, $timeout) {
+  var messages = [];
 
-    var cleanup = function () {
-      $timeout.cancel(reset);
-      reset = $timeout(resetfunc, 2000);
-    };
+  var reset;
+  var cleanup = function() {
+    $timeout.cancel(reset);
+    reset = $timeout(function() { messages = []; });
+  };
 
-    var resetfunc = function () {
-      alert("cleanup");
-      $scope.hmm = 'finished';
-      messages = [];
-    };
+  var emit = function() {
+    $rootScope.$emit('flash:message', messages, cleanup);
+  };
 
-    var emit = function () {
-      $rootScope.$emit('flash:message', messages, cleanup);
-    };
+  $rootScope.$on('$locationChangeSuccess', emit);
 
-    $rootScope.$on('$locationChangeSuccess', emit);
+  var asMessage = function(level, text) {
+    if (!text) {
+      text = level;
+      level = 'success';
+    }
+    return { level: level, text: text };
+  };
 
-    var asMessage = function (level, text) {
-      if (!text) {
-        text = level;
-        level = 'success';
-      }
-      return { level: level, text: text };
-    };
-
-    var asArrayOfMessages = function (level, text) {
-      if (level instanceof Array) return level.map(function (message) {
-        return message.text ? message : asMessage(message);
-      });
-      return text ? [
-        { level: level, text: text }
-      ] : [asMessage(level)];
-    };
-
-    var flash = function (level, text) {
-      emit(messages = asArrayOfMessages(level, text));
-    };
-
-    ['error', 'warning', 'info', 'success'].forEach(function (level) {
-      flash[level] = function (text) {
-        flash(level, text);
-      };
+  var asArrayOfMessages = function(level, text) {
+    if (level instanceof Array) return level.map(function(message) {
+      return message.text ? message : asMessage(message);
     });
+    return text ? [{ level: level, text: text }] : [asMessage(level)];
+  };
 
-    return flash;
-  }])
+  var flash = function(level, text) {
+    emit(messages = asArrayOfMessages(level, text));
+  };
 
-  .directive('flashMessages', [function () {
-    var directive = { restrict: 'EA', replace: true };
-    directive.template =
-      '<ol id="flash-messages">' +
-      '{{hmm}}'+
-        '<li ng-repeat="m in messages" class="{{m.level}}">{{m.text}}</li>' +
-        '</ol>';
+  ['error', 'warning', 'info', 'success'].forEach(function (level) {
+    flash[level] = function (text) { flash(level, text); };
+  });
 
-    directive.controller = ['$scope', '$rootScope', function ($scope, $rootScope) {
-      $rootScope.$on('flash:message', function (_, messages, done) {
-        $scope.messages = messages;
-        $scope.hmm= 'mytest';
-        done();
-      });
-    }];
+  return flash;
+}])
 
-    return directive;
-  }]);
+.directive('flashMessages', [function() {
+  var directive = { restrict: 'EA', replace: true };
+  directive.template =
+    '<ol id="flash-messages">' +
+      '<li ng-repeat="m in messages" class="{{m.level}}">{{m.text}}</li>' +
+    '</ol>';
+
+  directive.controller = ['$scope', '$rootScope', function($scope, $rootScope) {
+    $rootScope.$on('flash:message', function(_, messages, done) {
+      $scope.messages = messages;
+      done();
+    });
+  }];
+
+  return directive;
+}]);
