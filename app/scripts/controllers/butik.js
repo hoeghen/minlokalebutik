ba70632ea545979551b716709645179196348a2f
hoeghen <carverdk@yahoo.dk>
2015-08-04 20:58:13 +0200

    reformat

--- app/scripts/controllers/butik.js
+++ app/scripts/controllers/butik.js
@@ -2,87 +2,89 @@
 
 
 angular.module('testappApp')
-    .controller('ButikCtrl', function (dataService,$scope, $rootScope, $firebase, $http, $timeout, $filter, AlertService) {
-        var ref = new Firebase($rootScope.firebaseref);
-        var butikRef;
-        var alleTilbud = $firebase(ref.child("alletilbud")).$asArray();
+  .controller('ButikCtrl', function (dataService, $scope, $rootScope, $firebase, $http, $timeout, $filter, AlertService) {
+    var ref = new Firebase($rootScope.firebaseref);
+    var butikRef;
+    var alleTilbud = $firebase(ref.child("alletilbud")).$asArray();
 
-        $scope.butikfilter = {text:""};
-        $scope.alleTilbud = alleTilbud;
+    $scope.butikfilter = {text: ""};
+    $scope.alleTilbud = alleTilbud;
 
-        Number.prototype.roundTo = function (n) {
-            return Math.round(this / n) * n;
+    Number.prototype.roundTo = function (n) {
+      return Math.round(this / n) * n;
+    }
+
+    $rootScope.$watch('auth', function (newValue, oldValue) {
+      if (newValue !== oldValue) {
+        if ($rootScope.auth && $rootScope.auth.uid) {
+          butikRef = ref.child('users').child($rootScope.auth.uid).child("butik")
+          $scope.butik = $firebase(butikRef).$asObject();
         }
+      }
+    });
 
-        $rootScope.$watch('auth', function(newValue, oldValue) {
-          if(newValue !== oldValue){
-            if ($rootScope.auth && $rootScope.auth.uid) {
-              butikRef = ref.child('users').child($rootScope.auth.uid).child("butik")
-              $scope.butik = $firebase(butikRef).$asObject();
-            }
-          }
+
+    $scope.clearTilbud = function () {
+      var weekFromNow = new Date();
+      weekFromNow.setDate(weekFromNow.getDate() + 7);
+      $scope.tilbud = {slut: weekFromNow}
+
+    }
+
+    $scope.getAlert = AlertService.getAlert;
+
+
+    $scope.tabs = [
+      {active: true},
+      {active: false},
+      {active: false},
+    ];
+
+    $scope.saveShop = function () {
+      console.log("butik = " + $scope.butik);
+      var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+      url = url + $scope.butik.adresse + "&sensor=false";
+      console.log("URL =" + url);
+      $rootScope.busy = true;
+      $http({method: 'GET', url: url}).
+        success(function (data, status, headers, config) {
+          handleSuccessfullSave(data);
+        }).
+        error(function (data, status, headers, config) {
+          handleFailedSave(status);
         });
+    };
 
 
-        $scope.clearTilbud = function(){
-          var weekFromNow = new Date();
-          weekFromNow.setDate(weekFromNow.getDate()+7);
-          $scope.tilbud = {slut: weekFromNow}
+    $scope.addTilbud = function () {
+      var tilbud = $scope.tilbud;
+      if (tilbud.$id) {
+        var item = alleTilbud.$getRecord(tilbud.$id);
+        item.slut = angular.fromJson(angular.toJson(item.slut));
+        $scope.clearTilbud();
+        dataService.updateDistance(tilbud);
+        alleTilbud.$save(item).then(function (ref) {
+          AlertService.alert("Dit tilbud er updateret", "success", true);
+        }, function (cause) {
+          AlertService.alert("Kunne ikke gemme dit tilbud", "error", true);
+        });
+      } else {
+        tilbud.butik = $scope.butik;
+        $scope.clearTilbud();
+        tilbud.slut = angular.fromJson(angular.toJson(tilbud.slut));
+        dataService.updateDistance(tilbud);
+        alleTilbud.$add(tilbud).then(function (tilbudRef) {
+          if (!$scope.butik.tilbud) {
+            $scope.butik.tilbud = [];
+          }
+          $scope.butik.tilbud.push(tilbudRef.key());
+          saveTilbud();
+        }, function (reason) {
+          log.console("save tilbud failed:" + reason)
+        });
+      }
 
-        }
-
-        $scope.getAlert = AlertService.getAlert;
-
-
-        $scope.tabs = [
-            {active: true},
-            {active: false},
-            {active: false},
-        ];
-
-        $scope.saveShop = function () {
-            console.log("butik = " + $scope.butik);
-            var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
-            url = url + $scope.butik.adresse + "&sensor=false";
-            console.log("URL =" + url);
-            $rootScope.busy = true;
-            $http({method: 'GET', url: url}).
-                success(function (data, status, headers, config) {
-                handleSuccessfullSave(data);
-              }).
-                error(function (data, status, headers, config) {
-                handleFailedSave(status);
-              });
-        };
-
-
-        $scope.addTilbud = function () {
-            var tilbud = $scope.tilbud;
-            if(tilbud.$id){
-              var item = alleTilbud.$getRecord(tilbud.$id);
-              item.slut = angular.fromJson(angular.toJson(item.slut));
-              $scope.clearTilbud();
-              dataService.updateDistance(tilbud);
-              alleTilbud.$save(item).then(function(ref){
-                AlertService.alert("Dit tilbud er updateret", "success", true);
-              },function(cause){
-                AlertService.alert("Kunne ikke gemme dit tilbud", "error", true);
-              });
-            }else{
-              tilbud.butik = $scope.butik;
-              $scope.clearTilbud();
-              tilbud.slut = angular.fromJson(angular.toJson(tilbud.slut));
-              dataService.updateDistance(tilbud);
-              alleTilbud.$add(tilbud).then(function(tilbudRef){
-                if(!$scope.butik.tilbud){
-                  $scope.butik.tilbud = [];
-                }
-                $scope.butik.tilbud.push(tilbudRef.key());
-                saveTilbud();
-              },function(reason){log.console("save tilbud failed:"+reason)});
-            }
-
-       }
+    }
 
     function saveTilbud() {
       $scope.butik.$save().then(
@@ -97,47 +99,47 @@ angular.module('testappApp')
     }
 
     $scope.$watch('tilbud.forpris', function () {
-            if ($scope.tilbud.pris > 0 && $scope.tilbud.forpris > 0) {
-                $scope.tilbud.rabat = Number((($scope.tilbud.forpris - $scope.tilbud.pris) / $scope.tilbud.forpris * 100).toFixed(0));
-            }
-        });
-        $scope.$watch('tilbud.pris', function () {
-            if ($scope.tilbud.forpris > 0 && $scope.tilbud.forpris > 0) {
-                if ($scope.active == 'pris') {
-                    $scope.tilbud.rabat = Number((($scope.tilbud.forpris - $scope.tilbud.pris) / $scope.tilbud.forpris * 100).toFixed(0));
-                }
-            }
-        });
-        $scope.$watch('tilbud.rabat', function () {
-            if ($scope.tilbud.rabat > 0 && $scope.tilbud.forpris > 0) {
-                if ($scope.active == "rabat") {
-                    $scope.tilbud.pris = Number(($scope.tilbud.forpris - $scope.tilbud.forpris * $scope.tilbud.rabat / 100).toFixed(2)).roundTo(0.25);
-
-                }
-            }
-        });
-
-        $scope.focus = function (element) {
-            $scope.active = element;
+      if ($scope.tilbud.pris > 0 && $scope.tilbud.forpris > 0) {
+        $scope.tilbud.rabat = Number((($scope.tilbud.forpris - $scope.tilbud.pris) / $scope.tilbud.forpris * 100).toFixed(0));
+      }
+    });
+    $scope.$watch('tilbud.pris', function () {
+      if ($scope.tilbud.forpris > 0 && $scope.tilbud.forpris > 0) {
+        if ($scope.active == 'pris') {
+          $scope.tilbud.rabat = Number((($scope.tilbud.forpris - $scope.tilbud.pris) / $scope.tilbud.forpris * 100).toFixed(0));
         }
+      }
+    });
+    $scope.$watch('tilbud.rabat', function () {
+      if ($scope.tilbud.rabat > 0 && $scope.tilbud.forpris > 0) {
+        if ($scope.active == "rabat") {
+          $scope.tilbud.pris = Number(($scope.tilbud.forpris - $scope.tilbud.forpris * $scope.tilbud.rabat / 100).toFixed(2)).roundTo(0.25);
 
-        $scope.edit = function (index) {
-            $scope.tabs[1].active = true;
-            $scope.tilbud = alleTilbud.$getRecord($scope.butik.tilbud[index])
-            $scope.tilbud.slut = new Date($scope.tilbud.slut);
         }
-        $scope.remove = function (index) {
-            var id = $scope.butik.tilbud[index].$id;
-            var item = alleTilbud.$getRecord(id);
-            alleTilbud.$remove(item)
-            $scope.butik.tilbud.splice(index,1);
-            $scope.butik.$save();
-        }
+      }
+    });
 
-        $scope.getTilbud = function(){
+    $scope.focus = function (element) {
+      $scope.active = element;
+    }
+
+    $scope.edit = function (index) {
+      $scope.tabs[1].active = true;
+      $scope.tilbud = alleTilbud.$getRecord($scope.butik.tilbud[index])
+      $scope.tilbud.slut = new Date($scope.tilbud.slut);
+    }
+    $scope.remove = function (index) {
+      var id = $scope.butik.tilbud[index].$id;
+      var item = alleTilbud.$getRecord(id);
+      alleTilbud.$remove(item)
+      $scope.butik.tilbud.splice(index, 1);
+      $scope.butik.$save();
+    }
+
+    $scope.getTilbud = function () {
       var list = [];
-      if($scope.butik && $scope.butik.tilbud){
-        $scope.butik.tilbud.forEach(function(tilref){
+      if ($scope.butik && $scope.butik.tilbud) {
+        $scope.butik.tilbud.forEach(function (tilref) {
           var tilbud = alleTilbud.$getRecord(tilref);
           list.push(tilbud);
         })
