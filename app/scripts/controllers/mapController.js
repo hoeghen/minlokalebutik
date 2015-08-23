d547ba3fa984be1e64e89771099e5cebcf452f44
hoeghen <carverdk@yahoo.dk>
2015-08-23 08:47:19 +0200

    avoid location update when zooming, mindre email skrift

--- app/scripts/controllers/mapController.js
+++ app/scripts/controllers/mapController.js
@@ -8,32 +8,36 @@ angular.module('testappApp').controller('myMapController', function($scope,$root
   $scope.$parent.selectMarker = selectMarker;
   $scope.location = dataService.getCurrentPosition();
   var lockMap = false;
-
   var circle;
+  var isFitting = false;
 
   $scope.$watch('manueladresse', function () {
     dataService.setManualAdress($scope.manueladresse);
   });
 
 
-//  $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+
+  function addMapEvents() {
+    mapRef.addListener('zoom_changed', function() {
+      if (!isFitting) {
+        lockMap = true; // dont change map on location change when zooming
+      }
+    });
+  }
+
   $scope.$on('mapInitialized', function(event, map) {
     map.setOptions({disableDefaultUI:true,scrollwheel: false,zoomControl: true})
     mapRef = map;
-    addMapClickEvent()
+    addMapClickEvent();
+    addMapEvents();
   });
 
+  // Triggeren when a distance button is hit
   $scope.$watch('search',function(newValue, oldValue){
     lockMap = false;
     fitToCircle(newValue.distance,$scope.location);
   },true)
 
-  //$scope.$watch('search.dirty',function(newValue, oldValue){
-  //  $scope.search.dirty = false;
-  //  fitToCircle($scope.search.distance,$rootScope.location);
-  //  $scope.GenerateMapMarkers();
-  //},true)
-  //
   $scope.$watch('location.dirty',function(newValue, oldValue){
     if(!$scope.search.butik ){ // Dont update map if butik has been chosen or map is locked
       fitToCircle($scope.search.distance,$scope.location);
@@ -56,11 +60,13 @@ angular.module('testappApp').controller('myMapController', function($scope,$root
       }
 
       if(circle && radius && location.currentPosition){
+        isFitting = true;
         var center = new google.maps.LatLng(location.currentPosition.coords.latitude,location.currentPosition.coords.longitude);
         circle.setCenter(center);
         circle.setRadius(Number(radius));
         mapRef.fitBounds(circle.getBounds());
         mapRef.setZoom(mapRef.getZoom()+1);
+        isFitting = false;
       }
       $scope.GenerateMapMarkers();
     }
@@ -97,6 +103,7 @@ angular.module('testappApp').controller('myMapController', function($scope,$root
 
   function addClickEvent(marker) {
     google.maps.event.addListener(marker, 'click', function(){
+      isFitting = true;
       mapRef.setCenter(marker.position);
       mapRef.setZoom(17);
       infoWindow.open(mapRef,marker);
@@ -104,26 +111,31 @@ angular.module('testappApp').controller('myMapController', function($scope,$root
         $scope.search.butik = marker.tilbud.butik;
         dataService.setSearch($scope.search);
       });
+      isFitting = false;
     });
   }
 
   function addMapClickEvent() {
     google.maps.event.addListener(mapRef, 'click', function(){
       $rootScope.$apply(function() {
+        lockMap = false;
         $scope.search.butik = null;
         $scope.search.dirty = !$scope.search.dirty;
+        fitToCircle($scope.search.distance,$scope.location);
       })
-      fitToCircle($scope.search.distance,$scope.location);
+
     });
   }
 
   function selectMarker (tilbud) {
+      isFitting = true;
       mapRef.setCenter(tilbud.marker.position);
       mapRef.setZoom(17);
       infoWindow.close();
       infoWindow = createInfoWindow(tilbud.marker);
       infoWindow.open(mapRef,tilbud.marker);
       lockMap = true;
+      isFitting = false;
   }
 
 
