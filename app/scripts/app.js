16b32b3772727a14e47d7defb896ce6b23758902
hoeghen <carverdk@yahoo.dk>
2015-11-01 12:24:19 +0100

    version

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -50,6 +50,7 @@ angular.module('testappApp')
     run(function($rootScope) {
       $rootScope.firebaseref = 'https://minlokalebutik.firebaseio.com';
       $rootScope.baseUrl = "/";
+      $rootScope.version = "1";
   }
 );
 
